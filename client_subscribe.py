b5037d54f1e0007a823c107a1b1e30e4dfd52685
kevinusmayadhyw <57991938+kevinusmayadhyw@users.noreply.github.com>
2021-12-28 22:39:55 +0700

    Penyesuaian

--- client_subscribe.py
+++ client_subscribe.py
@@ -2,71 +2,41 @@
 import paho.mqtt.client as mqtt
 import time
 from prettytable import PrettyTable
-from tabulate import tabulate
 from IPython.display import clear_output
 import os
 
-# Function untuk Main Menu
-def menu():
-    t = PrettyTable(['No', 'Menu'])
-    t.add_row(['01', 'Subscribe ke Laundry Bojong (menggunakan port 2222)'])
-    t.add_row(['02', 'Subscribe ke Laundry Soang  (menggunakan port 3333)'])
-    t.add_row(['03', 'Subscribe untuk Perbandingan Waktu (menggunakan port 4444)'],)
-    t.add_row(['04', 'Keluar'])
-    t.align = 'l'
-    print(t)
-
-# Menu 1 untuk Laundry Bojong
 def laundrybojong():
-    # Simpan data laundry
-    list = []
+    databojong = []
 
-    # Function untuk memberikan pesan kepada client lain jika terconnect dengan laundry bojong
     def on_connect_laundrybojong(client, userdata, flags, rc):
-        time.sleep(1.5)
-        print('')
+        print("Menghubungkan ke Laundry Bojong dalam 3 detik")
+        for i in range(3):
+            print("...")
+            time.sleep(1)
         print('Terhubung ke Laundry Bojong...')
 
-    # Function untuk mengambil data dari laundry bojong, kemudian menampilkannya
     def on_message_laundrybojong(client, userdata, message):
         print('')
         data = str(message.payload.decode('utf-8')).split('|')
-        list.append(data)
+        databojong.append(data)
         clear_output(wait=True)
         os.system('cls')
-        print('+----------------------------------------------------------------------------------------------------------------------------------------------+')
-        print('|                                                              Data Laundry Bojong                                                             |')
-        nomer_list = range(1, len(list)+1)
-        print(tabulate(list, headers=[
-            '    Nama    ', 'Tanggal & Waktu Pengajuan', '  Berat  ', 'Jenis Paket', 'Penjemputan Cucian', 'Pengembalian Cucian', '      Total Harga      '], tablefmt='pretty', showindex=nomer_list))
+        print("====================================================================================================================================================")
+        print("==                                                                Data Laundry Bojong                                                             ==")
+        print("====================================================================================================================================================")
+        no = range(1, len(data)+1)
+        print("")
+        header=['    Nama    ', '     Waktu Pengajuan     ', '  Berat  ', 'Jenis Paket', '    Penjemputan   ', '    Pengembalian   ', '      Total Harga      ']
+        print(tabulate(data, header, tablefmt='fancy_grid', showindex=no))
 
-        # Membuat log
-        # text_file = open('laundrybojong.txt', 'a')
-        # # text_file.write(
-        # #     '+----------------------------------------------------------------------------------------------------------------------------------------------+'+'\n')
-        # # text_file.write(
-        # #     '|                                                              DATA LAUNDRY-AN BOJONG                                                          |'+'\n')
-        # # text_file.write(
-        # #     '+----------------------------------------------------------------------------------------------------------------------------------------------+'+'\n')
-        # # text_file.write('|     '+data[0]+'     |  '+data[1]+' |    '+data[2] + '    |    ' +
-        # #                 data[3]+'    |  '+data[4]+' |  '+data[5]+' |   '+data[6]+'  |'+'\n')
-        # text_file.write(data[0]+', '+data[1]+', '+data[2] + ', ' +
-        #                 data[3]+', '+data[4]+', '+data[5]+', '+data[6]+'\n')
-        # text_file.write(
-        #     '----------------------------------------------------------------------------------------------------------------------------------'+'\n')
-        # text_file.close()
-
-    # Set ip_broker & buat client
-    ip_broker = 'localhost'
+    broker_address = 'localhost'
     client = mqtt.Client('laundrybojong', clean_session=True)
 
-    # Mengirim pesan ketika connect dan mengambil pesan dari Laundry Bojong
     client.on_connect = on_connect_laundrybojong
     client.on_message = on_message_laundrybojong
-    client.connect(ip_broker, port=2222)
+    client.connect(broker_address, port=2222)
 
     client.loop_start()
-    # Topik = datalaundry
     client.subscribe('datalaundry', qos=1)
 
     while True:
@@ -74,59 +44,38 @@ def laundrybojong():
 
     client.loop_stop()
 
-
-
-# Menu 2 untuk Laundry Soang
 def laundrysoang():
-    # Simpan data laundry
-    list = []
+    datasoang = []
 
-    # Function untuk memberikan pesan kepada client lain jika terconnect dengan laundry soang
     def on_connect_laundrysoang(client, userdata, flags, rc):
-        time.sleep(1.5)
-        print('')
-        print('Terhubung ke Laundry Soang')
+        print("Menghubungkan ke Laundry Soang dalam 3 detik")
+        for i in range(3):
+            print("...")
+            time.sleep(1)
+        print('Terhubung ke Laundry Soang...')
 
-    # Function untuk mengambil data dari laundry soang, kemudian menampilkannya
     def on_message_laundrysoang(client, userdata, message):
         print('')
         data = str(message.payload.decode('utf-8')).split('|')
-        list.append(data)
+        datasoang.append(data)
         clear_output(wait=True)
         os.system('cls')
-        print('+----------------------------------------------------------------------------------------------------------------------------------------------+')
-        print('|                                                              Data Laundry Soang                                                              |')
-        nomer_list = range(1, len(list)+1)
-        print(tabulate(list, headers=[
-            '    Nama    ', 'Tanggal & Waktu Pengajuan', '  Berat  ', 'Jenis Paket', 'Penjemputan Cucian', 'Pengembalian Cucian', '      Total Harga      '], tablefmt='pretty', showindex=nomer_list))
+        print("====================================================================================================================================================")
+        print("==                                                                Data Laundry Soang                                                              ==")
+        print("====================================================================================================================================================")
+        no = range(1, len(datasoang)+1)
+        print("")
+        header=['    Nama    ', '     Waktu Pengajuan     ', '  Berat  ', 'Jenis Paket', '    Penjemputan   ', '    Pengembalian   ', '      Total Harga      ']
+        print(tabulate(datasoang, header, tablefmt='fancy_grid', showindex=no))
 
-        # Membuat log
-        # text_file = open('laundrysoang.txt', 'a')
-        # # text_file.write(
-        # #     '+----------------------------------------------------------------------------------------------------------------------------------------------+'+'\n')
-        # # text_file.write(
-        # #     '|                                                              DATA LAUNDRY-AN  SOANG                                                          |'+'\n')
-        # # text_file.write(
-        # #     '+----------------------------------------------------------------------------------------------------------------------------------------------+'+'\n')
-        # # text_file.write('|     '+data[0]+'     |  '+data[1]+' |    '+data[2] + '    |    ' +
-        # #                 data[3]+'    |  '+data[4]+' |  '+data[5]+' |   '+data[6]+'  |'+'\n')
-        # text_file.write(data[0]+', '+data[1]+', '+data[2] + ', ' +
-        #                 data[3]+', '+data[4]+', '+data[5]+', '+data[6]+'\n')
-        # text_file.write(
-        #     '----------------------------------------------------------------------------------------------------------------------------------'+'\n')
-        # text_file.close()
-
-    # Set ip_broker & buat client
-    ip_broker = 'localhost'
+    broker_address = 'localhost'
     client = mqtt.Client('laundrysoang', clean_session=True)
 
-    # Mengirim pesan ketika connect dan mengambil pesan dari laundry soang
     client.on_connect = on_connect_laundrysoang
     client.on_message = on_message_laundrysoang
-    client.connect(ip_broker, port=3333)
+    client.connect(broker_address, port=3333)
 
     client.loop_start()
-    # Topik = datalaundry
     client.subscribe('datalaundry', qos=1)
 
     while True:
@@ -134,59 +83,38 @@ def laundrysoang():
 
     client.loop_stop()
 
+def perbandingan():
+    dataperbandingan = []
 
-
-# Menu 3 untuk Banding Waktu
-def bandingwaktu():
-    # Simpan data banding waktu
-    list = []
-
-    # Function untuk memberikan pesan kepada client lain jika terconnect dengan banding waktu
     def on_connect_bandingwaktu(client, userdata, flags, rc):
-        time.sleep(1.5)
-        print('')
-        print('Terhubung ke Perbandingan Waktu')
+        print("Menghubungkan ke perbandingan dalam 3 detik")
+        for i in range(3):
+            print("...")
+            time.sleep(1)
+        print('Terhubung ke Perbandingan Laundry Bojong dan Soang...')
 
-    # Function untuk mengambil data dari banding waktu, kemudian menampilkannya
     def on_message_bandingwaktu(client, userdata, message):
         print('')
         data = str(message.payload.decode('utf-8')).split('|')
-        list.append(data)
+        dataperbandingan.append(data)
         clear_output(wait=True)
         os.system('cls')
-        print('+------------------------------------------------------------------------------------------------------------------------------------------------------+')
-        print('|                                                                  Perbandingan Waktu Laundry                                                          |')
-        print(tabulate(list, headers=[' Laundry ', '    Nama    ', 'Tanggal & Waktu Pengajuan', '  Berat  ',
-              'Jenis Paket', 'Penjemputan Cucian', 'Pengembalian Cucian', '      Total Harga      '], tablefmt='pretty'))
+        print("====================================================================================================================================================")
+        print("==                                                       Perbandingan Laundry Bojong dan Soang                                                    ==")
+        print("====================================================================================================================================================")
+        print("")
+        header=['    Nama    ', '     Waktu Pengajuan     ', '  Berat  ', 'Jenis Paket', '    Penjemputan   ', '    Pengembalian   ', '      Total Harga      ']
+        print(tabulate(dataperbandingan, header, tablefmt='fancy_grid'))
         print('')
 
-        # Membuat log
-        # text_file = open('bandingwaktu.txt', 'a')
-        # # text_file.write(
-        # #     '+----------------------------------------------------------------------------------------------------------------------------------------------+'+'\n')
-        # # text_file.write(
-        # #     '|                                                              DATA LAUNDRY-AN BOJONG                                                          |'+'\n')
-        # # text_file.write(
-        # #     '+----------------------------------------------------------------------------------------------------------------------------------------------+'+'\n')
-        # # text_file.write('|     '+data[0]+'     |  '+data[1]+' |    '+data[2] + '    |    ' +
-        # #                 data[3]+'    |  '+data[4]+' |  '+data[5]+' |   '+data[6]+'  |'+'\n')
-        # text_file.write(data[0]+', '+data[1]+', '+data[2] + ', ' +
-        #                 data[3]+', '+data[4]+', '+data[5]+', '+data[6]+', '+data[7]+'\n')
-        # text_file.write(
-        #     '----------------------------------------------------------------------------------------------------------------------------------'+'\n')
-        # text_file.close()
-
-    # Set ip_broker & buat client
-    ip_broker = 'localhost'
+    broker_address = 'localhost'
     client = mqtt.Client('BandingWaktu', clean_session=True)
 
-    # Mengirim pesan ketika connect dan mengambil pesan dari kedua laundry
     client.on_connect = on_connect_bandingwaktu
     client.on_message = on_message_bandingwaktu
-    client.connect(ip_broker, port=4444)
+    client.connect(broker_address, port=4444)
 
     client.loop_start()
-    # Topik = datalaundry
     client.subscribe('datalaundry', qos=1)
 
     while True:
@@ -195,42 +123,46 @@ def bandingwaktu():
     client.loop_stop()
 
 
+def menu():
+    berhenti = False
+    while not(berhenti):
+        print("=================================================")
+        print("==                   Laundry                   ==")
+        print("=================================================")
+        print("== NO |                  Menu                  ==")
+        print("== 1  | Laundry Bojong                         ==")
+        print("== 2  | Laundry Soang                          ==")
+        print("== 3  | Perbandingan Laundry Bojong dan Soang  ==")
+        print("== 4  | Keluar                                 ==")
+        print("=================================================")
+        pilihan = input("Masukan pilihan anda : ")
+        if pilihan == "1":
+            laundrybojong()
+        elif pilihan == "2":
+            laundrysoang()
+        elif pilihan == "3":
+            perbandingan()
+        elif pilihan == "4":
+            berhenti = True
+            print("...")
+            os.system("cls")
+            print("=================================================")
+            print("==             Anda Berhasil Keluar            ==")
+            print("=================================================")
+            print("Mematikan program dalam 5 detik")
+            for i in range(5):
+                print(i+1)
+                time.sleep(1)
 
-# Menu 4 untuk keluar dari program
-def exit():
-    time.sleep(3)
-    print('')
-    print('Keluar dari program...')
+            os.system("cls")
+        else:
+            print("Input yang anda masukan tidak tersedia")
+            print("Mengembalikan ke menu utama dalam 2 detik")
+            for i in range(2):
+                print(i+1)
+                time.sleep(1)
+            os.system("cls")
+            
 
-
-# Akses menuju main program, untuk memilih pilihan Laundry Bojong, Laundry Soang, Perbandingan Waktu, dan keluar dari program
 os.system('cls')
 menu()
-loop = True
-while loop:
-    pil = input('Masukan pilihan anda : ')
-    if pil == '01':
-        laundrybojong()
-    elif pil == '02':
-        laundrysoang()
-    elif pil == '03':
-        bandingwaktu()
-    elif pil == '04':
-        loop = False
-        exit()
-    else:
-        time.sleep(3)
-        print('')
-        print('+-------------------------------+')
-        print('|    Mohon maaf, input salah    |')
-        print('+-------------------------------+')
-        pil = input('Coba lagi? [Y/n] ')
-        time.sleep(3)
-        if pil == 'Y' or pil == 'y':
-            os.system('cls')
-            menu()
-        else:
-            loop = False
-            time.sleep(3)
-            print('')
-            print('Program berhenti')
